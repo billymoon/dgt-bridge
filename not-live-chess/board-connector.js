f85da7c73ddad77aa0778f7975a4e4f09e98d99d
Billy Moon <billy@itaccess.org>
2021-02-12 08:14:48 +0000

    wip

---
+++ not-live-chess/board-connector.js
@@ -0,0 +1,77 @@
+const SerialPort = require("serialport");
+const dgtMethods = require("./dgt-methods");
+
+const boardConnector = (port) =>
+  new Promise((resolve, reject) => {
+    const serialport = new SerialPort(port, {
+      baudRate: 9600,
+      autoOpen: false,
+      // endOnClose: true,
+      // lock: false
+    });
+
+    let chunks = Buffer.alloc(0);
+    const requests = [];
+
+    serialport.on("data", (payload) => {
+      if (payload) {
+        chunks = Buffer.concat([chunks, payload]);
+      }
+
+      while (
+        requests.length &&
+        requests[0].returnDataLength <= chunks.length
+      ) {
+        const request = requests.shift();
+        const value = chunks.slice(0, request.returnDataLength);
+        chunks = chunks.slice(request.returnDataLength);
+        request.callback(value);
+      }
+    });
+
+    const dgtMethodReducer = (
+      memo,
+      { methodName, messageTypeCode, returnDataLength, returnDataHandler }
+    ) => {
+      const method = returnDataLength
+        ? async () =>
+            new Promise((resolve) => {
+              serialport.write(Buffer.from(messageTypeCode, "hex"));
+              requests.push({
+                returnDataLength,
+                callback: (data) => resolve(returnDataHandler(data)),
+              });
+            })
+        : serialport.write(Buffer.from(messageTypeCode, "hex"));
+
+      return {
+        [methodName]: method,
+        ...memo,
+      };
+    };
+
+    const board = {
+      close: () => serialport.close(),
+      ...dgtMethods.reduce(dgtMethodReducer, {})
+    };
+
+    let tries = 15;
+    const openSerialPort = () => {
+      serialport.open((err) => {
+        if (tries--) {
+          if (err) {
+            console.log({ err });
+            setTimeout(openSerialPort, 2000);
+          } else {
+            resolve(board);
+          }
+        } else {
+          reject("ran out of tries connecting to socket");
+        }
+      });
+    };
+
+    openSerialPort();
+  });
+
+module.exports = boardConnector;
---
+++ not-live-chess/index.js
@@ -0,0 +1,40 @@
+const boardConnector = require("./board-connector");
+
+const loadBoard = async (port, callback) => {
+  const { reset, getBoard, getSerialNumber, getVersion } = await boardConnector(
+    port
+  );
+
+  await reset();
+  const serialNumber = await getSerialNumber();
+  const version = await getVersion();
+
+  console.log(
+    `Connected to board: ${JSON.stringify({ serialNumber, version })}`
+  );
+
+  let position = "";
+
+  const checkForNewPosition = async () => {
+    const nextPosition = JSON.stringify(await getBoard());
+    if (nextPosition !== position) {
+      position = nextPosition;
+      callback(
+        JSON.stringify({
+          timestamp: new Date().toISOString(),
+          serialNumber,
+          version,
+          position: JSON.parse(position),
+        })
+      );
+    }
+    setTimeout(checkForNewPosition, 200)
+  }
+
+  return {
+    serialNumber,
+    version
+  }
+};
+
+module.exports = loadBoard
